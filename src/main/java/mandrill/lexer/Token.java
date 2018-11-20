96bf7a52c4aabc04e77b2cada8b4da8c98626a2f
Marco Burstein <marco@marco.how>
2018-11-19 23:14:04 -0800

    Remove `TokenType` from `Token`.
    Use the `TokenType` class from the `TokenType.java` file, update
    `toString` to be more specific, and make the class public.

--- src/main/java/mandrill/lexer/Token.java
+++ src/main/java/mandrill/lexer/Token.java
@@ -1,8 +1,6 @@
 package mandrill.lexer;
 
-import java.util.regex.Pattern;
-
-class Token {
+public class Token {
     private TokenType tokenType;
     private Object value;
 
@@ -11,36 +9,20 @@ class Token {
         this.value = value;
     }
 
-    TokenType getTokenType() {
+    public TokenType getTokenType() {
         return tokenType;
     }
 
-    Object getValue() {
+    public Object getValue() {
         return value;
     }
 
     @Override
     public String toString() {
-        return tokenType.name() + ": " + value;
-    }
-}
+        if (value != null) {
+            return "[" + tokenType.name() + " " + value + "]";
+        }
 
-enum TokenType {
-    PUNCTUATOR("[()\\[\\]{},;]"),
-    OPERATOR ("\\+|\\-|\\*|\\/|\\^|%|=|\\+\\+|\\-\\-|\\+=|\\-=|\\*=|\\/=|\\^=|%="),
-    STRING_LITERAL("'([^'\\\\]|\\\\.)*'"),
-    NUMBER_LITERAL("([0-9]+(\\.[0-9]+)*)(?=[^0-9\\.])"),
-    IDENTIFIER("[_A-Za-z]+(?=[^_A-Za-z])"),
-    BOOLEAN_LITERAL("true(?=[^_A-Za-z])|false(?=[^_A-Za-z])"),
-    COMMENT("\\/\\/.*");
-
-    private String regexString;
-
-    TokenType(String regexString) {
-        this.regexString = regexString;
-    }
-
-    Pattern getRegexPattern() {
-        return Pattern.compile(regexString);
+        return "[" + tokenType.name() + "]";
     }
 }
