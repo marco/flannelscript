5a0da626e92b1f2f326e0ceebebdb1de8997333c
Marco Burstein <marco@marco.how>
2018-11-22 13:21:53 -0800

    Update the `Lexer` logic and regular expressions.
    Include whitespace at the beginning of regular expressions and only
    remove a substring when moving to the next token, in order to fix a bug
    in which semicolons would get absorbed into the previous token.
    
    Also, add a method `lexAndOutput` for outputting tokens without a
    prefix.

--- src/main/java/mandrill/lexer/Lexer.java
+++ src/main/java/mandrill/lexer/Lexer.java
@@ -1,11 +1,25 @@
 package mandrill.lexer;
 
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Lexer {
+    public static void main(String[] args) {
+        try {
+            byte[] inputBytes = Files.readAllBytes(Paths.get(args[0]));
+            String input = new String(inputBytes, Charset.forName("UTF-8"));
+            lexAndOutput(input);
+        } catch (Exception exception) {
+            System.out.println(exception);
+            System.exit(1);
+        }
+    }
+
     public static List<Token> lex(String input) {
         TokenType[] tokenTypes = TokenType.values();
         List<Token> currentTokenList = new ArrayList<Token>();
@@ -13,7 +27,6 @@ public class Lexer {
 
         for (int i = 0; i < input.length(); i++) {
             currentTokenString += input.charAt(i);
-
             for (int j = 0; j < tokenTypes.length; j++) {
                 Pattern pattern = tokenTypes[j].getRegexPattern();
                 Matcher matcher = pattern.matcher(currentTokenString);
@@ -32,7 +45,7 @@ public class Lexer {
                         value
                     );
                     currentTokenList.add(newToken);
-                    currentTokenString = "";
+                    currentTokenString = currentTokenString.substring(matcher.end());
                 }
             }
         }
@@ -43,5 +56,16 @@ public class Lexer {
     public static void lexAndPrint(String input) {
         System.out.println("Tokenizing: " + input);
         System.out.println(lex(input));
+    }
+
+    public static void lexAndOutput(String input) {
+        List<Token> result = lex(input);
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (Token token : result) {
+            stringBuilder.append(token.toString() + "\n");
+        }
+
+        System.out.println(stringBuilder.toString());
     }
 }
